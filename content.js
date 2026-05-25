a99e1922ad6ab9ffa711f479c93720fb04bbb4bb
guljar09 <amazingworldfact09@gmail.com>
2026-05-25 15:42:52 +0530

    Update alert message for connection confirmation

--- content.js
+++ content.js
@@ -1,10 +1,9 @@
-console.log("CONTENT CONNECTED");
-
 chrome.runtime.onMessage.addListener((req) => {
 
     if (req.action === "TEST") {
 
-        alert("Website Connected ✔️");
+        alert("CONNECTED ✔️");
 
     }
+
 });
