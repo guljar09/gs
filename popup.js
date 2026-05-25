b95a8a2b0c667cf1fe7c5821213114305a01ae08
guljar09 <amazingworldfact09@gmail.com>
2026-05-25 15:36:38 +0530

    Make button click handler asynchronous

--- popup.js
+++ popup.js
@@ -1,5 +1,12 @@
-document.getElementById("btn").addEventListener("click", () => {
+document.getElementById("btn").addEventListener("click", async () => {
 
-    alert("WORKING ✔️");
+    const [tab] = await chrome.tabs.query({
+        active: true,
+        currentWindow: true
+    });
+
+    chrome.tabs.sendMessage(tab.id, {
+        action: "TEST"
+    });
 
 });
